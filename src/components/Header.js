de6cc34ccb073d1a329e332ea843556319946bf8
Marcos Marti <marcos.marti12@gmail.com>
2020-05-13 19:45:08 -0400

    make it better

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Click headers to filter  or use the search box</p>
+      </div>
+    )
+  }
+
+export default Header;
